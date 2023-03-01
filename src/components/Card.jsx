90f0d8332d2e9c5484d82e697cff25cad7f0f806
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-03-02 00:48:34 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,15 @@
-
 import React from "react";
 
-function Card() {
+function Card(pooo) {
   return (
     <div className="card">
       <div className="top">
-        <h2 className="name">Beyonce</h2>
-        <img
-          className="circle-img"
-          src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-          alt="avatar_img"
-        />
+        <h2 className="name">{pooo.name}</h2>
+        <img className="circle-img" src={pooo.img} alt="avatar_img" />
       </div>
       <div className="bottom">
-        <p className="info">+123 456 789</p>
-        <p className="info">b@beyonce.com</p>
+        <p className="info">{pooo.tell}</p>
+        <p className="info">{pooo.mail}</p>
       </div>
     </div>
   );
