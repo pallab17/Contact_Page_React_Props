b32a0e59b62ca8f9f2d5c0ef1e48c7a261d20faf
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-03-02 00:48:02 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,31 @@
 import React from "react";
+import contacts from "../contacts";
+import Card from "./Card";
 
-function Card(pooo) {
+function App() {
   return (
-    <div className="card">
-      <div className="top">
-        <h2 className="name">{pooo.name}</h2>
-        <img className="circle-img" src={pooo.img} alt="avatar_img" />
-      </div>
-      <div className="bottom">
-        <p className="info">{pooo.tell}</p>
-        <p className="info">{pooo.mail}</p>
-      </div>
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tell={contacts[0].phone}
+        mail={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tell={contacts[1].phone}
+        mail={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tell={contacts[2].phone}
+        mail={contacts[2].email}
+      />
     </div>
   );
 }
 
-export default Card;
+export default App;
