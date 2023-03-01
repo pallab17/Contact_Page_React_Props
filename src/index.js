fbca798c63802f712a9f66643ea918f499cd7245
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-03-02 00:50:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-//1. Apply CSS styles to App.jsx component
+//1. Apply CSS styles to App.jsx component done
 //to match the appearance on the completed app:
 //https://c6fkx.csb.app/
-//2. Extract the contact card as a reusable Card component.
+//2. Extract the contact card as a reusable Card component. done
 //3. Use props to render the default Beyonce contact card
-//so the Card component can be reused for other contacts.
-//4. Import the contacts.js file to create card components.
+//so the Card component can be reused for other contacts. done
+//4. Import the contacts.js file to create card components.  done
